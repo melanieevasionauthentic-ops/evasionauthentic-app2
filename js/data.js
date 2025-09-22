cf4fc98e190290b1fdec37a22cc038161d05664e
melanieevasionauthentic-ops <melanieevasionauthentic@gmail.com>
2025-09-22 15:48:21 +0200

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -26,6 +26,14 @@ window.APP_DATA = {
       "lat": 39.7961,
       "lng": 2.6935
     },
+    "Pollença (Calvari)": {
+      "lat": 39.8766,
+      "lng": 3.0189
+    },
+    "Alcúdia": {
+      "lat": 39.8532,
+      "lng": 3.121
+    },
     "Mirador Es Colomer": {
       "lat": 39.9406,
       "lng": 3.1432
@@ -37,14 +45,6 @@ window.APP_DATA = {
     "Plage de Formentor": {
       "lat": 39.9276,
       "lng": 3.1476
-    },
-    "Pollença (Calvari)": {
-      "lat": 39.8766,
-      "lng": 3.0189
-    },
-    "Alcúdia": {
-      "lat": 39.8532,
-      "lng": 3.121
     },
     "Es Trenc": {
       "lat": 39.3338,
@@ -122,25 +122,29 @@ window.APP_DATA = {
       "lat": 39.4946,
       "lng": 2.8918
     },
-    "Lloret de Vistalegre": {
-      "lat": 39.6207,
-      "lng": 2.9852
+    "Sineu": {
+      "lat": 39.6411,
+      "lng": 3.0107
+    },
+    "Inca": {
+      "lat": 39.7186,
+      "lng": 2.9076
     },
     "Manacor": {
       "lat": 39.5696,
       "lng": 3.2096
     },
-    "Montuïri": {
-      "lat": 39.5589,
-      "lng": 2.9865
+    "Pollença": {
+      "lat": 39.8766,
+      "lng": 3.0189
     },
-    "Calvià": {
-      "lat": 39.5657,
-      "lng": 2.5064
+    "Port de Pollença (Pollença)": {
+      "lat": 39.9073,
+      "lng": 3.0817
     },
-    "s’Arenal (Llucmajor)": {
-      "lat": 39.4986,
-      "lng": 2.7544
+    "Algaida": {
+      "lat": 39.5535,
+      "lng": 2.8994
     },
     "Campanet": {
       "lat": 39.765,
@@ -150,125 +154,21 @@ window.APP_DATA = {
       "lat": 39.7039,
       "lng": 3.0126
     },
-    "Pina (Algaida)": {
-      "lat": 39.5937,
-      "lng": 2.9555
+    "Montuïri": {
+      "lat": 39.5589,
+      "lng": 2.9865
     },
-    "s’Alqueria Blanca (Santanyí)": {
-      "lat": 39.3864,
-      "lng": 3.187
-    },
-    "Peguera (Calvià)": {
-      "lat": 39.5378,
-      "lng": 2.4523
-    },
-    "Andratx": {
-      "lat": 39.5757,
-      "lng": 2.4206
-    },
-    "Sa Pobla": {
-      "lat": 39.7699,
-      "lng": 3.0231
-    },
-    "Port de Pollença (Pollença)": {
-      "lat": 39.9073,
-      "lng": 3.0817
-    },
-    "Sencelles": {
-      "lat": 39.6478,
-      "lng": 2.8977
-    },
-    "Sineu": {
-      "lat": 39.6411,
-      "lng": 3.0107
-    },
-    "Vilafranca de Bonany": {
-      "lat": 39.568,
-      "lng": 3.087
-    },
-    "Ariany": {
-      "lat": 39.6537,
-      "lng": 3.0949
-    },
-    "Calonge (Santanyí)": {
-      "lat": 39.3836,
-      "lng": 3.1967
-    },
-    "Es Llombards (Santanyí)": {
-      "lat": 39.3377,
-      "lng": 3.116
-    },
-    "Inca": {
-      "lat": 39.7186,
-      "lng": 2.9076
-    },
-    "Sant Llorenç des Cardassar": {
-      "lat": 39.6088,
-      "lng": 3.2844
+    "Alcúdia (ville)": {
+      "lat": 39.8532,
+      "lng": 3.121
     },
     "Port d’Alcúdia (Alcúdia)": {
       "lat": 39.8423,
       "lng": 3.1244
     },
-    "Son Carrió (Sant Llorenç)": {
-      "lat": 39.5908,
-      "lng": 3.2928
-    },
-    "Son Ferrer (Calvià)": {
-      "lat": 39.5086,
-      "lng": 2.4947
-    },
-    "Alaró": {
-      "lat": 39.7055,
-      "lng": 2.7909
-    },
-    "s’Arracó (Andratx)": {
-      "lat": 39.5666,
-      "lng": 2.41
-    },
-    "Biniali (Sencelles)": {
-      "lat": 39.6475,
-      "lng": 2.8362
-    },
-    "Búger": {
-      "lat": 39.7447,
-      "lng": 2.9855
-    },
-    "Cala Rajada (Capdepera)": {
-      "lat": 39.7158,
-      "lng": 3.4606
-    },
-    "Badia Gran (Llucmajor)": {
-      "lat": 39.4558,
-      "lng": 2.8047
-    },
-    "Puigpunyent": {
-      "lat": 39.6225,
-      "lng": 2.5272
-    },
-    "Santa Eugènia": {
-      "lat": 39.6411,
-      "lng": 2.8362
-    },
-    "Son Bugadelles (Calvià)": {
-      "lat": 39.5911,
-      "lng": 2.4957
-    },
-    "Santa Ponsa (Calvià)": {
-      "lat": 39.5087,
-      "lng": 2.4762
-    },
-    "Muro": {
-      "lat": 39.7366,
-      "lng": 3.0563
-    },
-    "Porto Cristo (Manacor)": {
-      "lat": 39.5397,
-      "lng": 3.332
-    },
-    "Pollença": {
-      "lat": 39.8766,
-      "lng": 3.0189
+    "Santanyí (centre)": {
+      "lat": 39.3546,
+      "lng": 3.1296
     }
   }
 };
