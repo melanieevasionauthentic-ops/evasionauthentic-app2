89864dcaf010db10066116aff63cf90b26b87098
melanieevasionauthentic-ops <melanieevasionauthentic@gmail.com>
2025-09-22 17:26:15 +0200

    Add files via upload

---
+++ js/data_fetes_patch.js
@@ -0,0 +1,292 @@
+/* Patch FÊTES */
+window.APP_DATA = window.APP_DATA || {};
+APP_DATA.fetes = [
+  {
+    "id": "rois-mages",
+    "titre": "Défilé des Rois mages",
+    "mois": "janvier",
+    "jour": "5–6 janvier",
+    "ville": "Palma",
+    "coords": [
+      39.5696,
+      2.6502
+    ],
+    "resume": "Grande cavalcade des Rois mages à Palma, chars illuminés et distribution de bonbons.",
+    "description": "La veille de l’Épiphanie, les Rois arrivent par la mer au Port de Palma puis défilent jusqu’au centre historique. C’est l’un des moments les plus attendus des familles.",
+    "images": [
+      "assets/fetes/rois_mages_palma.jpg"
+    ]
+  },
+  {
+    "id": "sant-antoni",
+    "titre": "Sant Antoni & dimonis",
+    "mois": "janvier",
+    "jour": "16–17 janvier",
+    "ville": "Sa Pobla / Artà / Pollença",
+    "coords": [
+      39.7691,
+      3.0217
+    ],
+    "resume": "Bénédiction des animaux, bûchers et parades de ‘démons’.",
+    "description": "Fête très populaire où les confréries défilent au son de la ximbomba. A Sa Pobla, le mât de cocagne (Pi) est le clou du spectacle.",
+    "images": [
+      "assets/fetes/sant_antoni_sa_pobla.jpg"
+    ]
+  },
+  {
+    "id": "sant-sebastia",
+    "titre": "Sant Sebastià – Palma",
+    "mois": "janvier",
+    "jour": "20 janvier",
+    "ville": "Palma",
+    "coords": [
+      39.5713,
+      2.6469
+    ],
+    "resume": "Concerts gratuits et barbecues de rue autour de grands brasiers.",
+    "description": "Le saint patron de Palma se célèbre avec des scènes autour de la Plaça Major et du Parc de la Mar.",
+    "images": [
+      "assets/fetes/correfoc_palma.jpg"
+    ]
+  },
+  {
+    "id": "carnaval",
+    "titre": "Carnaval – Sa Rua / Sa Rueta",
+    "mois": "février-mars",
+    "jour": "Week-end & mardi gras",
+    "ville": "Palma",
+    "coords": [
+      39.5696,
+      2.6502
+    ],
+    "resume": "Défilés colorés, chars et fanfares. Sa Rueta pour les enfants, Sa Rua pour les adultes.",
+    "description": "Le centre de Palma devient une grande scène carnavalesque avec concours de déguisements.",
+    "images": [
+      "assets/fetes/carnaval_palma.jpg"
+    ]
+  },
+  {
+    "id": "semaine-sainte",
+    "titre": "Semana Santa – Processions",
+    "mois": "mars-avril",
+    "jour": "Semaine sainte",
+    "ville": "Partout à Majorque",
+    "coords": [
+      39.61,
+      2.9
+    ],
+    "resume": "Processions nocturnes des confréries, encens et tambours.",
+    "description": "Les plus marquantes ont lieu à Palma et à Pollença, avec la procession du Vendredi Saint.",
+    "images": [
+      "assets/fetes/semaine_sainte_palma.jpg"
+    ]
+  },
+  {
+    "id": "fira-vi",
+    "titre": "Fira del Vi",
+    "mois": "avril",
+    "jour": "fin avril",
+    "ville": "Pollença",
+    "coords": [
+      39.8762,
+      3.0176
+    ],
+    "resume": "Salon des vins dans le cloître de Sant Domingo.",
+    "description": "Les bodegas de l’île présentent leurs cuvées DO Binissalem et Pla i Llevant.",
+    "images": [
+      "assets/fetes/patrona_pollenca.jpg"
+    ]
+  },
+  {
+    "id": "sa-fira-sineu",
+    "titre": "Sa Fira – grand marché aux bestiaux",
+    "mois": "mai",
+    "jour": "1er dimanche de mai",
+    "ville": "Sineu",
+    "coords": [
+      39.642,
+      3.01
+    ],
+    "resume": "Plus ancienne foire agricole de Majorque.",
+    "description": "Étalons, bœufs, artisans, gastronomie : l’île rurale se donne rendez-vous à Sineu.",
+    "images": [
+      "assets/fetes/cossiers_algaida.jpg"
+    ]
+  },
+  {
+    "id": "es-firo",
+    "titre": "Es Firó – Moros i Cristians",
+    "mois": "mai",
+    "jour": "2e week-end de mai",
+    "ville": "Sóller",
+    "coords": [
+      39.766,
+      2.715
+    ],
+    "resume": "Reconstitution de la victoire contre les corsaires (1561).",
+    "description": "Défilés, combats simulés et feu d’artifice sur la plage du Port de Sóller.",
+    "images": [
+      "assets/fetes/soller_moros_cristians.jpg"
+    ]
+  },
+  {
+    "id": "corpus",
+    "titre": "Corpus Christi",
+    "mois": "mai-juin",
+    "jour": "jeudi après la Trinité",
+    "ville": "Palma",
+    "coords": [
+      39.5696,
+      2.6502
+    ],
+    "resume": "Concerts dans les patios et processions traditionnelles.",
+    "description": "Patios ouverts et musique baroque au cœur du centre historique.",
+    "images": [
+      "assets/fetes/noel_palma.jpg"
+    ]
+  },
+  {
+    "id": "nit-sant-joan",
+    "titre": "Nit de Sant Joan – Correfoc",
+    "mois": "juin",
+    "jour": "24 juin (veille)",
+    "ville": "Palma & villages",
+    "coords": [
+      39.5696,
+      2.6502
+    ],
+    "resume": "Feux, diables et danses au Parc de la Mar.",
+    "description": "Entrée spectaculaire de l’été à Majorque avec correfoc et bains de minuit.",
+    "images": [
+      "assets/fetes/correfoc_palma.jpg"
+    ]
+  },
+  {
+    "id": "verge-del-carme",
+    "titre": "Verge del Carme",
+    "mois": "juillet",
+    "jour": "16 juillet",
+    "ville": "Ports de l’île",
+    "coords": [
+      39.55,
+      2.72
+    ],
+    "resume": "Processions maritimes en l’honneur des pêcheurs.",
+    "description": "Sorties en mer avec l’image de la Vierge, bénédictions des flottes.",
+    "images": [
+      "assets/fetes/noel_palma.jpg"
+    ]
+  },
+  {
+    "id": "sant-jaume-cossiers",
+    "titre": "Sant Jaume & Ball dels Cossiers",
+    "mois": "juillet",
+    "jour": "25 juillet",
+    "ville": "Algaida",
+    "coords": [
+      39.559,
+      2.892
+    ],
+    "resume": "Danse traditionnelle des Cossiers avec le démon.",
+    "description": "Six danseurs et la Dama protègent symboliquement le village.",
+    "images": [
+      "assets/fetes/cossiers_algaida.jpg"
+    ]
+  },
+  {
+    "id": "patrona-pollenca",
+    "titre": "Festes de la Patrona",
+    "mois": "août",
+    "jour": "2 août",
+    "ville": "Pollença",
+    "coords": [
+      39.8762,
+      3.0176
+    ],
+    "resume": "Grand combat Moros i Cristians et feux d’artifice.",
+    "description": "Commémoration de la victoire de 1550 menée par Joan Mas contre Dragut.",
+    "images": [
+      "assets/fetes/patrona_pollenca.jpg"
+    ]
+  },
+  {
+    "id": "minorque-gracia",
+    "titre": "Mare de Déu de Gràcia",
+    "mois": "septembre",
+    "jour": "9 septembre",
+    "ville": "Maó (Minorque)",
+    "coords": [
+      39.889,
+      4.265
+    ],
+    "resume": "Spectaculaires joutes et ‘bôts’ des chevaux minorquins.",
+    "description": "Clôture des fêtes patronales de Minorque, pure tradition équestre.",
+    "images": [
+      "assets/fetes/minorque_sant_joan_chevaux.jpg"
+    ]
+  },
+  {
+    "id": "verema",
+    "titre": "Festes de la Verema",
+    "mois": "septembre",
+    "jour": "fin septembre",
+    "ville": "Binissalem",
+    "coords": [
+      39.684,
+      2.839
+    ],
+    "resume": "Fête des vendanges, bataille de raisin et concerts.",
+    "description": "Ambiance bon enfant et vins DO Binissalem à l’honneur.",
+    "images": [
+      "assets/fetes/patrona_pollenca.jpg"
+    ]
+  },
+  {
+    "id": "fira-alcudia",
+    "titre": "Fira d’Alcúdia",
+    "mois": "octobre",
+    "jour": "1er week-end d’octobre",
+    "ville": "Alcúdia",
+    "coords": [
+      39.853,
+      3.121
+    ],
+    "resume": "Grande foire agricole et artisanale des remparts médiévaux.",
+    "description": "Parades historiques ‘Estol del Rei en Jaume’, gastronomie et stands.",
+    "images": [
+      "assets/fetes/patrona_pollenca.jpg"
+    ]
+  },
+  {
+    "id": "noel-palma",
+    "titre": "Noël à Palma – illuminations",
+    "mois": "décembre",
+    "jour": "tout le mois",
+    "ville": "Palma",
+    "coords": [
+      39.5696,
+      2.6502
+    ],
+    "resume": "Marchés et illuminations (Plaça Major & Plaça d’Espanya).",
+    "description": "Ambiance chaleureuse et patinoire, parfaite pour une escapade hivernale.",
+    "images": [
+      "assets/fetes/noel_palma.jpg"
+    ]
+  },
+  {
+    "id": "estendard",
+    "titre": "Festa de l’Estendard",
+    "mois": "décembre",
+    "jour": "31 décembre",
+    "ville": "Palma",
+    "coords": [
+      39.5696,
+      2.6502
+    ],
+    "resume": "Commémoration de la reconquête (1229).",
+    "description": "Procession civico-religieuse et danse traditionnelle sur la Plaça Cort.",
+    "images": [
+      "assets/fetes/noel_palma.jpg"
+    ]
+  }
+];
